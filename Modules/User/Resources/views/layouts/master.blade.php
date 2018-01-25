cf48b4a73d43c66e2766da1760273214a8fc4d2b
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-01-25 12:11:06 +0700

    menyesuaikan layout

--- Modules/User/Resources/views/layouts/master.blade.php
+++ Modules/User/Resources/views/layouts/master.blade.php
@@ -13,56 +13,60 @@
         <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h5 class="display-4">Fluid jumbotron</h5>
-                    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                </div>
+        <div class="jumbotron jumbotron-fluid" style="background-color: white">
+            <div class="container">
+                <a href="{{ route('logout') }}" class="btn btn-outline-danger float-right" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                <h2>Sistem Informasi Manajemen Rumah Sakit</h2>
             </div>
-            @if($errors->any())
-                <div class="alert alert-danger alert-dismissable">
-                    @foreach($errors->all() as $error)
-                        <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                        <p>{{ $error }}</p>
-                    @endforeach
-                </div>
-            @endif
+        </div>
+        <div class="container-fluid">
+            <div class="col-md-12">
+                <div class="row">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                    @if($errors->any())
+                        <div class="alert alert-danger alert-dismissable">
+                            @foreach($errors->all() as $error)
+                                <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                <p>{{ $error }}</p>
+                            @endforeach
+                        </div>
+                    @endif
 
-            @if(Session::has('message'))
-                <div class="alert alert-success alert-dismissable">
-                    <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    {{ Session::get('message') }}
-                </div>
-            @endif
+                    @if(Session::has('message'))
+                        <div class="alert alert-success alert-dismissable">
+                            <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            {{ Session::get('message') }}
+                        </div>
+                    @endif
 
-            @if(Session::has('warning'))
-                <div class="alert alert-warning alert-dismissable">
-                    <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    {{ Session::get('warning') }}
-                </div>
-            @endif
-            <div class="row">
-                <div class="col-3">
-                    <div class="card card-body">
-                        <div class="list-group list-group-flush">
-                            <a class="list-group-item list-group-item-light" href="/">Beranda</a>
-                            <a class="list-group-item list-group-item-primary" href="{{ route('user.index') }}">Manajemen Akun Staff</a>
-                            <a class="list-group-item list-group-item-light">Pengaturan Lantai RS</a>
-                            <a class="list-group-item list-group-item-light">Pengaturan Kamar RS</a>
+                    @if(Session::has('warning'))
+                        <div class="alert alert-warning alert-dismissable">
+                            <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            {{ Session::get('warning') }}
+                        </div>
+                    @endif
+                    <div class="col-md-3">
+                        <div class="card card-body">
+                            <div class="list-group list-group-flush">
+                                <a class="list-group-item list-group-item-light" href="/">Beranda</a>
+                                <a class="list-group-item list-group-item-primary" href="{{ route('user.index') }}">Manajemen Akun Staff</a>
+                                <a class="list-group-item list-group-item-light">Pengaturan Lantai RS</a>
+                                <a class="list-group-item list-group-item-light">Pengaturan Kamar RS</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-9">
-                    <div class="row">
-                        <div class="card card-body">
-                            @yield('content')
+                    <div class="col-md-9">
+                        <div class="row">
+                            <div class="card card-body">
+                                @yield('content')
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
         <script src="{{ asset('bootstrap/js/app.js') }}"></script>
     </body>
 </html>
