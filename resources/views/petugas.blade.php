aa30bf47c6fc99d407974b195cf0bc55bd56de46
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-01-26 19:53:05 +0700

    update link url untuk pengaturan akun

--- resources/views/petugas.blade.php
+++ resources/views/petugas.blade.php
@@ -7,11 +7,11 @@
 @section('content')
     <div class="col-4">
         <div class="card">
-            <img class="img-fluid mx-auto" src="{{ asset('img/cog.png') }}" alt="Users">
+            <img class="img-fluid mx-auto" src="{{ asset('img/cog.png') }}" alt="Setting">
             <div class="card-body">
                 <h5 class="card-title">Pengaturan Akun</h5>
-                <p class="card-text"></p>
-                <a href="" class="btn btn-outline-primary">Go somewhere</a>
+                <p class="card-text">Melihat serta melakukan perubahan detail akun.</p>
+                <a href="{{ route('saya.index') }}" class="btn btn-outline-primary">Masuk</a>
             </div>
         </div>
     </div>
