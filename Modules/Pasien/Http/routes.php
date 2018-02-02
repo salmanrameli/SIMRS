94fd74cd593a0831e9b1be9976fae867ecdfcbba
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-02-02 14:43:55 +0700

    memasang middleware sehingga hanya administrator yang bisa mengakses

--- Modules/Pasien/Http/routes.php
+++ Modules/Pasien/Http/routes.php
@@ -2,10 +2,12 @@
 
 Route::group(['middleware' => 'web', 'namespace' => 'Modules\Pasien\Http\Controllers'], function()
 {
-    Route::get('/pasien/cari', [
-        'as' => 'pasien.cari',
-        'uses' => 'PasienController@cari'
-    ]);
+    Route::group(['middleware' => 'checkRole:administrator'], function() {
+        Route::get('/pasien/cari', [
+            'as' => 'pasien.cari',
+            'uses' => 'PasienController@cari'
+        ]);
 
-    Route::resource('pasien', 'PasienController');
+        Route::resource('pasien', 'PasienController');
+    });
 });
