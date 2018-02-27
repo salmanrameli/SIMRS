634334062bffebbb1a73ecc8b7f4f423f17e1c8a
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-02-27 20:12:13 +0700

    menyesuaikan layout

--- Modules/Pasien/Resources/views/index.blade.php
+++ Modules/Pasien/Resources/views/index.blade.php
@@ -6,46 +6,54 @@
 
 @section('content')
     <div class="card card-body">
-        <div class="col-md-12">
-            <form class="form-inline" action="{{ route('pasien.cari') }}" method="get">
-                <label for="cari" class="control-label">Cari Pasien: </label>
-                &nbsp;&nbsp;
-                <input type="text" class="form-control" id="query" name="query" placeholder="John Doe">
-                &nbsp;
-                <button type="submit" class="btn btn-primary">Cari</button>
-            </form>
+        <div class="row">
+            <div class="col-md-12">
+                <form class="form-inline" action="{{ route('pasien.cari') }}" method="get">
+                    <label for="cari" class="control-label">Cari Pasien: </label>
+                    &nbsp;&nbsp;
+                    <input type="text" class="form-control" id="query" name="query" placeholder="John Doe">
+                    &nbsp;
+                    <button type="submit" class="btn btn-primary">Cari</button>
+                </form>
+            </div>
         </div>
     </div>
-    <div class="card card-body">
-       <div class="col-md-12">
-           <a href="{{ route('pasien.create') }}" class="btn btn-outline-primary">Daftarkan Pasien Baru</a>
-           <br>
-           <br>
-           <nav>
-               <ul class="pagination justify-content-end float-left">
-                   {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
-               </ul>
-           </nav>
-           <table class="table">
-               <tr>
-                   <th>KTP</th>
-                   <th>Nama</th>
-                   <th>Alamat</th>
-                   <th></th>
-               </tr>
-               @foreach($pasiens as $pasien)
-                   <tr>
-                       <td>{{ $pasien->ktp }}</td>
-                       <td>{{ $pasien->nama }}</td>
-                       <td>{{ $pasien->alamat }}</td>
-                       <td><a href="{{ route('pasien.show', ['id' => $pasien->id]) }}" class="btn btn-outline-info">Lihat</a></td>
-                   </tr>
-               @endforeach
-           </table>
-           <ul class="pagination justify-content-end float-left">
-               {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
-           </ul>
-       </div>
+    <div class="col-md-12">
+        <div class="row">
+            <div class="card card-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="{{ route('pasien.create') }}" class="btn btn-outline-primary">Daftarkan Pasien Baru</a>
+                        <br>
+                        <br>
+                        <nav>
+                            <ul class="pagination justify-content-end float-left">
+                                {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
+                            </ul>
+                        </nav>
+                        <table class="table">
+                            <tr>
+                                <th>KTP</th>
+                                <th>Nama</th>
+                                <th>Alamat</th>
+                                <th></th>
+                            </tr>
+                            @foreach($pasiens as $pasien)
+                                <tr>
+                                    <td>{{ $pasien->ktp }}</td>
+                                    <td>{{ $pasien->nama }}</td>
+                                    <td>{{ $pasien->alamat }}</td>
+                                    <td><a href="{{ route('pasien.show', ['id' => $pasien->id]) }}" class="btn btn-outline-info">Lihat</a></td>
+                                </tr>
+                            @endforeach
+                        </table>
+                        <ul class="pagination justify-content-end float-left">
+                            {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
