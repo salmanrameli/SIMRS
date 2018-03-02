cc64e07aee00e012310f628321a166c9b7dcab11
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-03-02 19:09:24 +0700

    membuat modul rawat inap

--- Modules/User/Resources/views/homepage/administrasi.blade.php
+++ Modules/User/Resources/views/homepage/administrasi.blade.php
@@ -1,14 +1,15 @@
 @extends('layouttemplate::master')
 
 @section('content')
-    <div class="col-md-3">
-        <div class="card">
-            {{--<img class="img-fluid mx-auto" src="{{ asset('img/users.png') }}" alt="Users">--}}
-            <div class="card-body">
-                <h5 class="card-title">Administrasi Rawat Inap Pasien</h5>
-                <p class="card-text"></p>
-                <a href="{{ route('ranap.index') }}" class="btn btn-outline-primary">Masuk</a>
-            </div>
+    <div class="col-md-12">
+        <div class="card card-body">
+           <div class="col-md-12">
+               <div class="page-header">
+                   <h4>Administrasi Rawat Inap Pasien</h4>
+                   <hr>
+               </div>
+               <a href="{{ route('ranap.create') }}" class="btn btn-outline-primary">Rawat Inap Baru</a>
+           </div>
         </div>
     </div>
 @endsection
