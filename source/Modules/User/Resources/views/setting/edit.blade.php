ffecf89a8fabf5f54384f510436eff77d4df60a2
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-03-18 10:36:55 +0700

    tambah input id_user untuk dipakai controller waktu update

--- source/Modules/User/Resources/views/setting/edit.blade.php
+++ source/Modules/User/Resources/views/setting/edit.blade.php
@@ -12,6 +12,11 @@
         {{ Form::model($user, ['method' => 'PATCH', 'route' => ['setting.update', $user->id]]) }}
 
         <div class="form-group">
+            {{ Form::label('id_user', 'ID', ['class' => 'control-label']) }}
+            {{ Form::text('id_user', null, ['class' => 'form-control']) }}
+        </div>
+
+        <div class="form-group">
             {{ Form::label('nama', 'Nama', ['class' => 'control-label']) }}
             {{ Form::text('nama', null, ['class' => 'form-control']) }}
         </div>
@@ -27,11 +32,13 @@
         </div>
 
         @if($user->jabatan_id == '1')
-            <label name="jabatan_id">Jabatan</label><br>
-            @foreach($jabatans as $jabatan)
-                <input type="radio" name="jabatan_id" value="{{ $jabatan->id }}" {{ $user->jabatan_id == $jabatan->id ? 'checked' : '' }} > {{ ucfirst($jabatan->nama )}}<br>
-            @endforeach
-            <br>
+            <div class="form-group">
+                <label name="jabatan_id">Jabatan</label><br>
+                @foreach($jabatans as $jabatan)
+                    <input type="radio" name="jabatan_id" value="{{ $jabatan->id }}" style="" {{ $user->jabatan_id == $jabatan->id ? 'checked' : ''}} > {{ ucfirst($jabatan->nama )}}<br>
+                @endforeach
+                <br>
+            </div>
         @endif
 
         <br>
