4b454c7a7f47d2704728afab26f68a115d4919e3
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-04-23 19:41:08 +0700

    mengatur ulang rute pencarian pasien untuk menghilangkan bug halaman yang dituju tidak ditemukan

--- source/Modules/Pasien/Http/routes.php
+++ source/Modules/Pasien/Http/routes.php
@@ -2,10 +2,10 @@
 
 Route::group(['middleware' => 'web', 'namespace' => 'Modules\Pasien\Http\Controllers'], function()
 {
-    Route::resource('pasien', 'PasienController');
-
     Route::get('/pasien/cari', [
         'as' => 'pasien.cari',
         'uses' => 'PasienController@cari'
     ]);
+
+    Route::resource('pasien', 'PasienController');
 });
