4a67a6deef6abb159d18e0749cdcdcb5df3ea862
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-06-12 12:58:32 +0700

    refaktor route & nama fungsi supaya sesuai dengan class diagram

--- source/Modules/Obat/Http/Controllers/ObatController.php
+++ source/Modules/Obat/Http/Controllers/ObatController.php
@@ -22,7 +22,7 @@ class ObatController extends Controller
      * Display a listing of the resource.
      * @return Response
      */
-    public function index()
+    public function showAllObat()
     {
         $obat = Obat::all();
 
@@ -33,7 +33,7 @@ class ObatController extends Controller
      * Show the form for creating a new resource.
      * @return Response
      */
-    public function create()
+    public function registerNewObat()
     {
         return view('obat::create');
     }
@@ -43,7 +43,7 @@ class ObatController extends Controller
      * @param  Request $request
      * @return Response
      */
-    public function store(Request $request)
+    public function saveNewObat(Request $request)
     {
         $this->validate($request, [
             'nama' => 'required',
@@ -63,7 +63,7 @@ class ObatController extends Controller
      * Show the specified resource.
      * @return Response
      */
-    public function show($id)
+    public function showDetailObat($id)
     {
         $obat = Obat::findorFail($id);
 
@@ -74,7 +74,7 @@ class ObatController extends Controller
      * Show the form for editing the specified resource.
      * @return Response
      */
-    public function edit($id)
+    public function editObat($id)
     {
         $obat = Obat::findorFail($id);
 
@@ -86,7 +86,7 @@ class ObatController extends Controller
      * @param  Request $request
      * @return Response
      */
-    public function update(Request $request, $id)
+    public function updateObat(Request $request, $id)
     {
         $this->validate($request, [
             'nama' => 'required',
--- source/Modules/Obat/Http/routes.php
+++ source/Modules/Obat/Http/routes.php
@@ -2,5 +2,35 @@
 
 Route::group(['middleware' => 'web', 'namespace' => 'Modules\Obat\Http\Controllers'], function()
 {
-    Route::resource('/obat', 'ObatController');
+    //Route::resource('/obat', 'ObatController');
+
+    Route::get('/obat', [
+        'as' => 'obat.index',
+        'uses' => 'ObatController@showAllObat'
+    ]);
+
+    Route::get('/obat/create', [
+        'as' => 'obat.create',
+        'uses' => 'ObatController@registerNewObat'
+    ]);
+
+    Route::post('/obat', [
+        'as' => 'obat.store',
+        'uses' => 'ObatController@saveNewObat'
+    ]);
+
+    Route::get('/obat/{id}', [
+        'as' => 'obat.show',
+        'uses' => 'ObatController@showDetailObat'
+    ]);
+
+    Route::get('/obat/{id}/edit', [
+        'as' => 'obat.edit',
+        'uses' => 'ObatController@editObat'
+    ]);
+
+    Route::patch('/obat/{id}', [
+        'as' => 'obat.update',
+        'uses' => 'ObatController@updateObat'
+    ]);
 });
