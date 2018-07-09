2d8cdc54376aee06a3e2c3e9a454ebdebf631452
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-07-09 10:35:00 +0700

    ubah id elemen untuk tambah atribut nav-link active di sidebar

--- source/Modules/LayoutTemplate/Resources/views/attributes/bangunan.blade.php
+++ source/Modules/LayoutTemplate/Resources/views/attributes/bangunan.blade.php
@@ -3,7 +3,7 @@
     $('#manajemen_staff_jabatan').removeAttr("class", "active").attr("class", "nav-link");
     $('#manajemen_data_pasien').removeAttr("class", "active").attr("class", "nav-link");
     $('#manajemen_dokter').removeAttr("class", "active").attr("class", "nav-link");
-    $('#pengaturan_rumah_sakit').attr("class", "nav-link active").css("min-width", "201px");
+    $('#denah_ruangan').attr("class", "nav-link active").css("min-width", "201px");
     $('#manajemen_obat').removeAttr("class", "active").attr("class", "nav-link");
     $('#manajemen_alkes').removeAttr("class", "active").attr("class", "nav-link");
 </script>
