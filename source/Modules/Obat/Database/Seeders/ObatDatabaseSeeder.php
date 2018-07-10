8590df32fdcbf2055223f550a955fa068e1a2ea0
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-07-10 19:40:49 +0700

    tambah seeder database obat

--- source/Modules/Obat/Database/Seeders/ObatDatabaseSeeder.php
+++ source/Modules/Obat/Database/Seeders/ObatDatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Modules\Obat\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Faker\Factory as Faker;
+use Modules\Obat\Entities\Obat;
 
 class ObatDatabaseSeeder extends Seeder
 {
@@ -15,6 +17,35 @@ class ObatDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $item)
+        {
+            $obat = new Obat();
+            $obat->nama = $faker->company;
+            $obat->harga = rand(5, 100);
+
+            $rand1 = mt_rand(0, 4);
+            if($rand1 == 0)
+            {
+                $obat->tipe_obat = 'injeksi';
+            }
+            if($rand1 == 1)
+            {
+                $obat->tipe_obat = 'oral';
+            }
+            if($rand1 == 2)
+            {
+                $obat->tipe_obat = 'kompress';
+            }
+            else
+            {
+                $obat->tipe_obat = 'suppositoria';
+            }
+            $obat->save();
+
+        }
 
         // $this->call("OthersTableSeeder");
     }
