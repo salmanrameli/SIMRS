eee01db7d6d8ca6924be82e7f92627bf25d1e5ad
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-07-12 21:36:24 +0700

    membuat memasukkan catatan di view perjalanan_penyakit.show lewat popup modal

--- source/Modules/PerjalananPenyakit/Resources/views/show.blade.php
+++ source/Modules/PerjalananPenyakit/Resources/views/show.blade.php
@@ -1,102 +1,150 @@
-@extends('layouttemplate::master')
+@extends('layouttemplate::master-ranap')
 
 @section('title')
     Perjalanan Penyakit Pasien
 @endsection
 
 @section('content')
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs small">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="{{ route('perjalanan_penyakit.index', $id_ranap) }}">Perjalanan Penyakit</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('perintah_dokter_dan_pengobatan.index', $id_ranap) }}">Perintah Dokter dan Pengobatan</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('catatan_harian_perawatan.index', $id_ranap) }}">Catatan Harian dan Perawatan</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="card-body">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        @if(Auth::user()->jabatan_id == 4)
-                            <div class="float-right"><a href="{{ route('perjalanan_penyakit.create', $id_ranap) }}" class="btn btn-outline-primary">Buat Catatan Perjalanan Penyakit Baru</a></div>
-                        @endif
+    <div class="card-body">
+        <div class="col-md-12">
+            <div class="page-header">
+                @if(Auth::user()->jabatan_id == 4)
+                    <button type="button" class="btn btn-outline-primary float-right" data-toggle="modal" data-target="#modalBuatPerjalananPenyakit">Buat Catatan Perjalanan Penyakit Baru</button>
+                @endif
 
-                        <h4>Perjalanan Penyakit: {{ $perjalanan->rawat_inap->pasien->nama }}</h4>
-                        <hr>
-                        <div class="col-md-12">
-                            <table>
-                                <tbody class="small">
-                                    <tr>
-                                        <th>Jenis Kelamin</th>
-                                        <td style="padding-left: 10px">: {{ ucfirst($perjalanan->rawat_inap->pasien->jenkel) }}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Umur</th>
-                                        <td id="umur" style="padding-left: 10px"></td>
-                                    </tr>
-                                    <tr>
-                                        <th>Tanggal Masuk</th>
-                                        <td style="padding-left: 10px">: {{ date("d F Y", strtotime($perjalanan->rawat_inap->tanggal_masuk)) }}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Diagnosa Awal</th>
-                                        <td style="padding-left: 10px">: {{ ucfirst($perjalanan->rawat_inap->diagnosa_awal) }}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <br>
-                            <p id="tanggal_lahir" hidden>{{ $perjalanan->rawat_inap->pasien->tanggal_lahir }}</p>
-                        </div>
-                    </div>
-                    <table class="table small">
-                        <thead>
-                            <tr>
-                                <th>Perjalanan Penyakit</th>
-                                <th>Perintah Dokter dan Pengobatan</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td class="text-justify w-50">
-                                    <b>Dibuat tanggal: {{ date("d F Y", strtotime($perjalanan->tanggal_keterangan)) }}</b><br>
-                                    @if(strtotime($perjalanan->created_at) == strtotime($perjalanan->updated_at))
-                                        <b>Diubah tanggal: –</b>
-                                    @else
-                                        <b>Diubah tanggal: {{ date("d F Y", strtotime($perjalanan->updated_at)) }}</b>
-                                    @endif
-                                    <hr>
-                                    <label><b>Subjektif</b></label>
-                                    <p>{!! $perjalanan->subjektif !!}</p>
-                                    <label><b>Objektif</b></label>
-                                    <p>{!! $perjalanan->objektif !!}</p>
-                                    <label><b>Assessment</b></label>
-                                    <p>{!! $perjalanan->assessment !!}</p>
-                                </td>
-                                <td class="text-justify">
-                                    <label><b>Planning</b></label>
-                                    <p>{!! $perjalanan->planning_perintah_dokter_dan_pengobatan !!} &nbsp;<a href="{{ route('perintah_dokter_dan_pengobatan.show', [$id_ranap, $perjalanan->id]) }}">Pengobatan...</a></p>
-                                    @if(Auth::user()->jabatan_id ==4)
-                                        <hr>
-                                        <a href="{{ route('perjalanan_penyakit.edit', [$id_ranap, $perjalanan->id]) }}" class="btn btn-sm btn-warning float-right">Ubah</a>
-                                        @endif
-                                </td>
-                            </tr>
+                <h4>Perjalanan Penyakit: {{ $perjalanan->rawat_inap->pasien->nama }}</h4>
+                <hr>
+                <div class="col-md-12">
+                    <table>
+                        <tbody class="small">
+                        <tr>
+                            <th>Jenis Kelamin</th>
+                            <td style="padding-left: 10px">: {{ ucfirst($perjalanan->rawat_inap->pasien->jenkel) }}</td>
+                        </tr>
+                        <tr>
+                            <th>Umur</th>
+                            <td id="umur" style="padding-left: 10px"></td>
+                        </tr>
+                        <tr>
+                            <th>Tanggal Masuk</th>
+                            <td style="padding-left: 10px">: {{ date("d F Y", strtotime($perjalanan->rawat_inap->tanggal_masuk)) }}</td>
+                        </tr>
+                        <tr>
+                            <th>Diagnosa Awal</th>
+                            <td style="padding-left: 10px">: {{ ucfirst($perjalanan->rawat_inap->diagnosa_awal) }}</td>
+                        </tr>
                         </tbody>
                     </table>
+                    <br>
+                    <p id="tanggal_lahir" hidden>{{ $perjalanan->rawat_inap->pasien->tanggal_lahir }}</p>
                 </div>
+            </div>
+            <table class="table small">
+                <thead>
+                <tr>
+                    <th>Perjalanan Penyakit</th>
+                    <th>Perintah Dokter dan Pengobatan</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td class="text-justify w-50">
+                        <b>Dibuat tanggal: {{ date("d F Y", strtotime($perjalanan->tanggal_keterangan)) }}</b><br>
+                        @if(strtotime($perjalanan->created_at) == strtotime($perjalanan->updated_at))
+                            <b>Diubah tanggal: –</b>
+                        @else
+                            <b>Diubah tanggal: {{ date("d F Y", strtotime($perjalanan->updated_at)) }}</b>
+                        @endif
+                        <hr>
+                        <label><b>Subjektif</b></label>
+                        <p>{!! $perjalanan->subjektif !!}</p>
+                        <label><b>Objektif</b></label>
+                        <p>{!! $perjalanan->objektif !!}</p>
+                        <label><b>Assessment</b></label>
+                        <p>{!! $perjalanan->assessment !!}</p>
+                    </td>
+                    <td class="text-justify">
+                        <label><b>Planning</b></label>
+                        <p>{!! $perjalanan->planning_perintah_dokter_dan_pengobatan !!} &nbsp;<a href="{{ route('perintah_dokter_dan_pengobatan.show', [$ranap->id, $perjalanan->id]) }}">Pengobatan...</a></p>
+                        @if(Auth::user()->jabatan_id ==4)
+                            <hr>
+                            <a href="{{ route('perjalanan_penyakit.edit', [$ranap->id, $perjalanan->id]) }}" class="btn btn-sm btn-warning float-right">Ubah</a>
+                        @endif
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalBuatPerjalananPenyakit" tabindex="-1" role="dialog" aria-labelledby="modalBuatPerjalananPenyakit" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalBuatPerjalananPenyakit">Catatan Harian Perawatan Pasien: {{ $ranap->pasien->nama }}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+
+                {{ Form::open(['method' => 'POST', 'route' => ['perjalanan_penyakit.store', $ranap->id]]) }}
+                <div class="modal-body">
+                    <div hidden>
+                        {{ Form::text('id_ranap', $ranap->id) }}
+                    </div>
+
+                    <div class="form-group">
+                        <label for="datepicker" id="tanggal_keterangan" class="control-label">Tanggal</label>
+                        <input type="text" id="datepicker" name="tanggal_keterangan" class="form-control" placeholder="yyyy-mm-dd">
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('subjektif', 'Subjektif', ['class' => 'control-label']) }}
+                        {{ Form::textarea('subjektif', null, ['class' => 'form-control']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('objektif', 'Objektif', ['class' => 'control-label']) }}
+                        {{ Form::textarea('objektif', null, ['class' => 'form-control']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('assessment', 'Assessment', ['class' => 'control-label']) }}
+                        {{ Form::textarea('assessment', null, ['class' => 'form-control']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('planning_perintah_dokter_dan_pengobatan', 'Planning / Perintah Dokter dan Pengobatan', ['class' => 'control-label']) }}
+                        {{ Form::textarea('planning_perintah_dokter_dan_pengobatan', null, ['class' => 'form-control']) }}
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    {{ Form::submit('Simpan', ['class' => 'btn btn-outline-success']) }}
+                </div>
+                {{ Form::close() }}
+
             </div>
         </div>
     </div>
+
 @endsection
 
 @section('script')
+    <script>
+        $(document).ready(function () {
+            $('#modalBuatPerjalananPenyakit').on('shown.bs.modal', function () {
+                $("#subjektif").htmlarea();
+                $("#objektif").htmlarea();
+                $("#assessment").htmlarea();
+                $("#planning_perintah_dokter_dan_pengobatan").htmlarea();
+            })
+        });
+
+        $(function () {
+            $("#datepicker").datepicker({
+                dateFormat: 'yy-mm-dd'
+            });
+        });
+    </script>
     <script>
         var lahir = new Date($('#tanggal_lahir').text());
         var sekarang = new Date();
