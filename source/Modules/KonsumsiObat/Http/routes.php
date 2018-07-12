1d22cfa0663910c5b577abe6eb4d189148f5c726
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-07-12 14:51:15 +0700

    hapus rute & file create_konsumsi_obat_waktu karena sudah digantikan oleh popup modal di konsumsi_obat.index

--- source/Modules/KonsumsiObat/Http/routes.php
+++ source/Modules/KonsumsiObat/Http/routes.php
@@ -17,26 +17,6 @@ Route::group(['middleware' => 'web', 'namespace' => 'Modules\KonsumsiObat\Http\C
         'uses' => 'KonsumsiObatController@storeKonsumsiObat'
     ]);
 
-    Route::get('ranap/{id}/konsumsi_obat/{id_konsumsi_obat}/create_pagi', [
-        'as' => 'konsumsi_obat_pagi.create',
-        'uses' => 'KonsumsiObatController@createNewKonsumsiObatPagi'
-    ]);
-
-    Route::get('ranap/{id}/konsumsi_obat/{id_konsumsi_obat}/create_siang', [
-        'as' => 'konsumsi_obat_siang.create',
-        'uses' => 'KonsumsiObatController@createNewKonsumsiObatSiang'
-    ]);
-
-    Route::get('ranap/{id}/konsumsi_obat/{id_konsumsi_obat}/create_sore', [
-        'as' => 'konsumsi_obat_sore.create',
-        'uses' => 'KonsumsiObatController@createNewKonsumsiObatSore'
-    ]);
-
-    Route::get('ranap/{id}/konsumsi_obat/{id_konsumsi_obat}/create_malam', [
-        'as' => 'konsumsi_obat_malam.create',
-        'uses' => 'KonsumsiObatController@createNewKonsumsiObatMalam'
-    ]);
-
     Route::post('ranap/simpan_rincian_konsumsi_obat', [
         'as' => 'rincian_konsumsi_obat.store',
         'uses' => 'KonsumsiObatController@storeRincianKonsumsiObat'
