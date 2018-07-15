077f12b4508130971445532aa2b14224ca864659
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-07-15 15:54:32 +0700

    tambah icon setting di master template

--- source/Modules/LayoutTemplate/Resources/views/master.blade.php
+++ source/Modules/LayoutTemplate/Resources/views/master.blade.php
@@ -27,7 +27,7 @@
     <body style="background-image: url({{ asset('img/grey.png') }})">
         <div class="container-fluid" style="padding: 1% 20px 1% 20px; background-color: white; margin-bottom: 15px; border-bottom: 1px solid lightgrey;">
             <a href="{{ route('logout') }}" class="btn btn-outline-danger float-right" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-            <a href="{{ route('setting.index') }}" class="btn btn-outline-info float-right" style="margin-right: 10px">Pengaturan Akun</a>
+            <a href="{{ route('setting.index') }}" class="btn btn-outline-info float-right" style="margin-right: 10px"><i class="fa fa-cogs"></i> Pengaturan Akun</a>
             <h4>Sistem Informasi Manajemen Rumah Sakit | <small style="font-size: 17px">{{ \Illuminate\Support\Facades\Auth::user()->nama }}</small></h4>
         </div>
         <div class="container-fluid">
