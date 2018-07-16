e293243cd4fb74420bbd2bdbcb18942a4c46570a
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-07-16 19:43:32 +0700

    tambah tombol untuk mengubah keterangan konsumsi obat

--- source/Modules/KonsumsiObat/Resources/views/index.blade.php
+++ source/Modules/KonsumsiObat/Resources/views/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-md-12">
             <div class="page-header">
                 @if(Auth::user()->jabatan_id == 3)
-                <button type="button" class="btn btn-outline-primary float-right" data-toggle="modal" data-id-ranap="{{ $ranap->id }}" data-target="#modalCatatanHariPerawatanBaru">Catatan Hari Perawatan Baru</button>
+                    <button type="button" class="btn btn-outline-primary float-right" data-toggle="modal" data-id-ranap="{{ $ranap->id }}" data-target="#modalCatatanHariPerawatanBaru">Buat Catatan Hari Perawatan Baru</button>
                 @endif
                 <h4>Terapi / Penatalaksanaan Pasien: {{ $ranap->pasien->nama }}</h4>
                 <hr>
@@ -48,7 +48,7 @@
                     <tr>
                         <th colspan="3" class="text-center table-info">Tanggal</th>
                         <td colspan="4" class="text-center table-info">{{ date("d F Y", strtotime($hari->tanggal)) }}</td>
-                        <th rowspan="5" class="align-middle text-center">Keterangan</th>
+                        <th rowspan="5" class="align-middle text-center" width="20%">Keterangan</th>
                     </tr>
                     <tr>
                         <th colspan="3" class="text-center">Hari Perawatan</th>
@@ -79,7 +79,7 @@
                             <td class="text-center">
                                 @if(empty($obat->konsumsi_obat_pagi->sudah))
                                     @if(Auth::user()->jabatan_id == 3)
-                                        <button type="button" class="btn btn-default" data-toggle="modal" data-id-obat="{{ $obat->id }}" data-target="#modalKonsumsiPagi" style="width: 100%"><i class="fa fa-plus-circle"></i></button>
+                                        <button type="button" class="btn btn-sm btn-default" data-toggle="modal" data-id-obat="{{ $obat->id }}" data-target="#modalKonsumsiPagi" style="width: 100%"><i class="fa fa-plus-circle"></i></button>
                                     @endif
                                 @else
                                     <i class="fa fa-check"></i>
@@ -88,7 +88,7 @@
                             <td class="text-center">
                                 @if(empty($obat->konsumsi_obat_siang->sudah))
                                     @if(Auth::user()->jabatan_id == 3)
-                                        <button type="button" class="btn btn-default" data-toggle="modal" data-id-obat="{{ $obat->id }}" data-target="#modalKonsumsiSiang" style="width: 100%"><i class="fa fa-plus-circle"></i></button>
+                                        <button type="button" class="btn btn-sm btn-default" data-toggle="modal" data-id-obat="{{ $obat->id }}" data-target="#modalKonsumsiSiang" style="width: 100%"><i class="fa fa-plus-circle"></i></button>
                                     @endif
                                 @else
                                     <i class="fa fa-check"></i>
@@ -97,7 +97,7 @@
                             <td class="text-center">
                                 @if(empty($obat->konsumsi_obat_sore->sudah))
                                     @if(Auth::user()->jabatan_id == 3)
-                                        <button type="button" class="btn btn-default" data-toggle="modal" data-id-obat="{{ $obat->id }}" data-target="#modalKonsumsiSore" style="width: 100%"><i class="fa fa-plus-circle"></i></button>
+                                        <button type="button" class="btn btn-sm btn-default" data-toggle="modal" data-id-obat="{{ $obat->id }}" data-target="#modalKonsumsiSore" style="width: 100%"><i class="fa fa-plus-circle"></i></button>
                                     @endif
                                 @else
                                     <i class="fa fa-check"></i>
@@ -106,7 +106,7 @@
                             <td class="text-center">
                                 @if(empty($obat->konsumsi_obat_malam->sudah))
                                     @if(Auth::user()->jabatan_id == 3)
-                                        <button type="button" class="btn btn-default" data-toggle="modal" data-id-obat="{{ $obat->id }}" data-target="#modalKonsumsiMalam" style="width: 100%"><i class="fa fa-plus-circle"></i></button>
+                                        <button type="button" class="btn btn-sm btn-default" data-toggle="modal" data-id-obat="{{ $obat->id }}" data-target="#modalKonsumsiMalam" style="width: 100%"><i class="fa fa-plus-circle"></i></button>
                                     @endif
                                 @else
                                     <i class="fa fa-check"></i>
@@ -115,9 +115,17 @@
                             <td class="text-center">
                                 @if($obat->keterangan)
                                     {{ ucfirst($obat->keterangan) }}
+                                    <div class="btn-group float-right">
+                                        <button type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle dropdown-toggle-split small" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <span class="sr-only">Toggle Dropright</span>
+                                        </button>
+                                        <div class="dropdown-menu dropdown-menu-right">
+                                            <a class="nav-link" data-toggle="modal" data-id-keterangan="{{ $obat->id }}" data-keterangan="{{ $obat->keterangan }}" data-target="#modalUbahKeteranganObat">Ubah</a>
+                                        </div>
+                                    </div>
                                 @else
                                     @if(Auth::user()->jabatan_id == 3)
-                                        <button type="button" class="btn btn-default" data-toggle="modal" data-id-keterangan="{{ $obat->id }}" data-target="#modalTambahKeteranganObat" style="width: 100%"><i class="fa fa-plus"></i></button>
+                                        <button type="button" class="btn btn-sm btn-default" data-toggle="modal" data-id-keterangan="{{ $obat->id }}" data-target="#modalTambahKeteranganObat" style="width: 100%"><i class="fa fa-plus"></i></button>
                                         @else
                                         -
                                     @endif
@@ -219,6 +227,40 @@
                 </div>
                 <div class="modal-footer">
                     {{ Form::submit('Simpan', ['class' => 'btn btn-outline-success']) }}
+                </div>
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalUbahKeteranganObat" tabindex="-1" role="dialog" aria-labelledby="modalUbahKeteranganObat" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalUbahKeteranganObat">Ubah Keterangan Konsumsi Obat</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                {{ Form::open(['method' => 'PATCH', 'route' => 'konsumsi_obat.update_keterangan']) }}
+                <div class="modal-body">
+                    <div class="form-group" hidden>
+                        <label for="id_ranap" class="control-label">ID Ranap:</label>
+                        <input type="text" name="id_ranap" id="id_ranap" value="{{ $ranap->id }}">
+                    </div>
+
+                    <div class="form-group" hidden>
+                        <label for="id_konsumsi_obat" class="control-label">ID Konsumsi Obat:</label>
+                        <input type="text" name="id_konsumsi_obat" id="id_konsumsi_obat">
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('keterangan', 'Keterangan', ['class' => 'control-label']) }}
+                        {{ Form::text('keterangan', null, ['class' => 'form-control']) }}
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    {{ Form::submit('Simpan Perubahan', ['class' => 'btn btn-outline-success']) }}
                 </div>
                 {{ Form::close() }}
             </div>
@@ -391,7 +433,11 @@
 
             $('#modalTambahKeteranganObat').on("show.bs.modal", function (e) {
                 $("#modalTambahKeteranganObat").find('#id_konsumsi_obat').val($(e.relatedTarget).data('id-keterangan'));
-                $("#modalTambahKeteranganObat").find('#keterangan').val($(e.relatedTarget).data('keterangan'));
+            });
+
+            $('#modalUbahKeteranganObat').on("show.bs.modal", function (e) {
+                $("#modalUbahKeteranganObat").find('#id_konsumsi_obat').val($(e.relatedTarget).data('id-keterangan'));
+                $("#modalUbahKeteranganObat").find('#keterangan').val($(e.relatedTarget).data('keterangan'));
             });
 
             $('#modalKonsumsiPagi').on("show.bs.modal", function (e) {
