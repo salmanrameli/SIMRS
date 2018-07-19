ad3543956a923f677eba7109e0103ff61288b14a
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-07-19 21:54:17 +0700

    tambah library khill/lavacharts untuk menggambar grafik

--- source/config/app.php
+++ source/config/app.php
@@ -164,8 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
-
+        \Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -229,7 +228,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
+        'Lava' => \Khill\Lavacharts\Laravel\LavachartsFacade::class,
     ],
 
 ];
