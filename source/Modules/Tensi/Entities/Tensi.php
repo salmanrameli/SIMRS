a2a0253da252f33c5a158a146dbfc629fdba2dfd
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-07-20 19:13:55 +0700

    hapus file migrasi tabel tensi karena tabel tensi tidak digunakan

--- source/Modules/Tensi/Entities/Tensi.php
+++ source/Modules/Tensi/Entities/Tensi.php
@@ -6,9 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tensi extends Model
 {
-    protected $table = 'tensi';
-
-    protected $fillable = [
-        'id_ranap', 'id_hari_perawatan', 'id_petugas'
-    ];
+    protected $fillable = [];
 }
