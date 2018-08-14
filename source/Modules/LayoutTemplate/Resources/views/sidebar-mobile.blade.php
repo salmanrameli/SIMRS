614724a6542641606b8a719a47150c9f23fabefb
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-08-14 11:36:44 +0700

    hapus id nav-link sidebar mobile

--- source/Modules/LayoutTemplate/Resources/views/sidebar-mobile.blade.php
+++ source/Modules/LayoutTemplate/Resources/views/sidebar-mobile.blade.php
@@ -1,48 +1,48 @@
 @if(Auth::user()->jabatan_id == 1)
     <li class="nav-item active">
-        <a href="{{ route('user.index') }}" class="nav-link" id="manajemen_staff_jabatan" style="min-width: 200px"><i class="fas fa-users-cog"></i> Manajemen Staff</a>
+        <a href="{{ route('user.index') }}" class="nav-link" style="min-width: 200px"><i class="fas fa-users-cog"></i> Manajemen Staff</a>
     </li>
     <li class="dropdown-divider"></li>
     <li class="nav-item active">
-        <a href="{{ route('dokter.index') }}" class="nav-link" id="manajemen_dokter" style="min-width: 200px"><i class="fas fa-user-md"></i> Manajemen Dokter</a>
+        <a href="{{ route('dokter.index') }}" class="nav-link" style="min-width: 200px"><i class="fas fa-user-md"></i> Manajemen Dokter</a>
     </li>
     <li class="dropdown-divider"></li>
     <li class="nav-item active">
-        <a href="{{ route('pasien.index') }}" class="nav-link" id="manajemen_data_pasien" style="min-width: 200px"><i class="fas fa-heartbeat"></i> Manajemen Pasien</a>
+        <a href="{{ route('pasien.index') }}" class="nav-link" style="min-width: 200px"><i class="fas fa-heartbeat"></i> Manajemen Pasien</a>
     </li>
     <li class="dropdown-divider"></li>
     <li class="nav-item active">
-        <a href="{{ route('bangunan.index') }}" class="nav-link" id="denah_ruangan" style="min-width: 200px"><i class="fas fa-hospital"></i> Manajemen Bangunan</a>
+        <a href="{{ route('bangunan.index') }}" class="nav-link" style="min-width: 200px"><i class="fas fa-hospital"></i> Manajemen Bangunan</a>
     </li>
     <li class="dropdown-divider"></li>
     <li class="nav-item active">
-        <a href="{{ route('obat.index') }}" class="nav-link" id="manajemen_obat" style="min-width: 200px"><i class="fas fa-pills"></i> Manajemen Obat</a>
+        <a href="{{ route('obat.index') }}" class="nav-link" style="min-width: 200px"><i class="fas fa-pills"></i> Manajemen Obat</a>
     </li>
     <li class="dropdown-divider"></li>
     <li class="nav-item active">
-        <a href="{{ route('alat_kesehatan.index') }}" class="nav-link" id="manajemen_alkes" style="min-width: 200px"><i class="fas fa-toolbox"></i> Manajemen Alat Kesehatan</a>
+        <a href="{{ route('alat_kesehatan.index') }}" class="nav-link" style="min-width: 200px"><i class="fas fa-toolbox"></i> Manajemen Alat Kesehatan</a>
     </li>
     @endif
 
 @if(Auth::user()->jabatan_id == 2)
     <li class="nav-item active">
-        <a href="{{ route('ranap.index') }}" class="nav-link" id="pasien_ranap"><i class="fas fa-procedures"></i> Pasien Rawat Inap</a>
+        <a href="{{ route('ranap.index') }}" class="nav-link"><i class="fas fa-procedures"></i> Pasien Rawat Inap</a>
     </li>
     <li class="dropdown-divider"></li>
     <li class="nav-item active">
-        <a href="{{ route('bangunan.index') }}" class="nav-link" id="denah_ruangan">Ruangan</a>
+        <a href="{{ route('bangunan.index') }}" class="nav-link">Ruangan</a>
     </li>
     @endif
 
 @if(Auth::user()->jabatan_id == 3)
     <li class="nav-item active">
-        <a href="{{ route('ranap.index') }}" class="nav-link" id="pasien_ranap"><i class="fas fa-procedures"></i> Pasien Rawat Inap</a>
+        <a href="{{ route('ranap.index') }}" class="nav-link"><i class="fas fa-procedures"></i> Pasien Rawat Inap</a>
     </li>
     @endif
 
 @if(Auth::user()->jabatan_id == 4)
     <li class="nav-item active">
-        <a href="{{ route('ranap.index') }}" class="nav-link" id="pasien_ranap"><i class="fas fa-procedures"></i> Pasien Rawat Inap</a>
+        <a href="{{ route('ranap.index') }}" class="nav-link"><i class="fas fa-procedures"></i> Pasien Rawat Inap</a>
     </li>
     @endif
 
