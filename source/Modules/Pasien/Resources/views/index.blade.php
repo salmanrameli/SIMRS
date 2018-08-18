a5b35d85014c5caae4acedc83cecda450829fcc9
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2018-08-18 20:03:31 +0700

    update tampilan desktop & mobile modul pasien

--- source/Modules/Pasien/Resources/views/index.blade.php
+++ source/Modules/Pasien/Resources/views/index.blade.php
@@ -1,141 +1,46 @@
 @extends('layouttemplate::master')
 
 @section('title')
-    Manajemen Pasien
+    @if(Auth::user()->jabatan_id == 1)
+        Manajemen Pasien
+    @else
+        Pasien
+    @endif
 @endsection
 
 @section('content')
-    @if(Auth::user()->jabatan_id == 1)
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h3>Manajemen Pasien</h3>
-                        <br>
-                    </div>
-                </div>
+    <div class="card card-body">
+        <form class="form-inline" action="{{ route('pasien.cari') }}" method="get">
+            <label for="cari" class="control-label">Cari Pasien: </label>
+            &nbsp;&nbsp;
+            <input type="text" class="form-control" id="query" name="query" placeholder="John Doe">
+
+            <div class="d-none d-sm-block">
+                &nbsp;<button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Cari Pasien</button>
+
             </div>
-        </div>
-    @endif
-    <div class="col-md-12">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="d-none d-sm-block">
-                    <div class="card card-body">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <form class="form-inline" action="{{ route('pasien.cari') }}" method="get">
-                                    <label for="cari" class="control-label">Cari Pasien: </label>
-                                    &nbsp;&nbsp;
-                                    <input type="text" class="form-control" id="query" name="query" placeholder="John Doe">
-                                    &nbsp;
-                                    <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Cari Pasien</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+            <div class="d-sm-none">
+                <br>
+                <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Cari Pasien</button>
             </div>
-        </div>
+        </form>
     </div>
 
-    <div class="col-md-12">
-        <div class="row">
-            <div class="col-md-12 d-none d-sm-block">
-                <div class="card card-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a href="{{ route('pasien.create') }}" class="btn btn-outline-primary">Daftarkan Pasien Baru</a>
-                            <br>
-                            <br>
-                            <nav>
-                                <ul class="pagination justify-content-end float-left">
-                                    {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
-                                </ul>
-                            </nav>
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>Nama</th>
-                                        <th>Alamat</th>
-                                        <th>Telepon</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($pasiens as $pasien)
-                                    <tr>
-                                        <td>{{ ucwords($pasien->nama) }}</td>
-                                        <td>{{ ucwords($pasien->alamat) }}</td>
-                                        <td>{{ $pasien->telepon }}</td>
-                                        <td>
-                                            <div class="btn-group">
-                                                <a href="{{ route('pasien.show', ['id' => $pasien->id]) }}" class="btn btn-outline-info">Rincian Pasien</a>
-                                                <button type="button" class="btn btn-outline-info btn-sm dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <span class="sr-only">Toggle Dropdown</span>
-                                                </button>
-                                                <div class="dropdown-menu dropdown-menu-right">
-                                                    @if(Auth::user()->jabatan_id == 1 || Auth::user()->jabatan_id == 2)
-                                                        <a class="dropdown-item" href="{{ route('pasien.edit', ['id' => $pasien->id]) }}">Ubah</a>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            <ul class="pagination justify-content-end float-left">
-                                {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
-
-@section('content-mobile')
-    @if(Auth::user()->jabatan_id == 1)
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h3>Manajemen Pasien</h3>
-                        <br>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endif
-    <div class="d-block d-sm-none">
-        <div class="card card-body">
-            <div class="row">
-                <div class="col-md-12">
-                    <form class="form-inline" action="{{ route('pasien.cari') }}" method="get">
-                        <label for="cari" class="control-label">Cari Pasien: </label>
-                        <input type="text" class="form-control" id="query" name="query" placeholder="John Doe">
-                        <br><br><br>
-                        <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Cari Pasien</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="d-block d-sm-none">
-        <div class="card card-body">
+    <div class="card card-body">
+        <div class="table-responsive-sm">
             <a href="{{ route('pasien.create') }}" class="btn btn-outline-primary">Daftarkan Pasien Baru</a>
-            <br>
+            <hr>
             <nav>
                 <ul class="pagination justify-content-end float-left">
                     {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
                 </ul>
             </nav>
-            <table class="table small">
+            <table class="table table-striped">
                 <thead>
                     <tr>
                         <th>Nama</th>
+                        <th class="d-none d-md-block">Alamat</th>
                         <th>Telepon</th>
                         <th></th>
                     </tr>
@@ -144,8 +49,22 @@
                 @foreach($pasiens as $pasien)
                     <tr>
                         <td>{{ ucwords($pasien->nama) }}</td>
+                        <td class="d-none d-md-block">{{ ucwords($pasien->alamat) }}</td>
                         <td>{{ $pasien->telepon }}</td>
-                        <td>
+                        <td class="d-none d-lg-block">
+                            <div class="btn-group">
+                                <a href="{{ route('pasien.show', ['id' => $pasien->id]) }}" class="btn btn-outline-info">Rincian Pasien</a>
+                                <button type="button" class="btn btn-outline-info btn-sm dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    @if(Auth::user()->jabatan_id == 1 || Auth::user()->jabatan_id == 2)
+                                        <a class="dropdown-item" href="{{ route('pasien.edit', ['id' => $pasien->id]) }}">Ubah</a>
+                                    @endif
+                                </div>
+                            </div>
+                        </td>
+                        <td class="d-lg-none">
                             <div class="btn-group">
                                 <a href="{{ route('pasien.show', ['id' => $pasien->id]) }}" class="btn btn-sm btn-outline-info">Rincian<br>Pasien</a>
                                 <button type="button" class="btn btn-outline-info btn-sm dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -162,14 +81,12 @@
                 @endforeach
                 </tbody>
             </table>
-            <nav>
-                <ul class="pagination justify-content-end float-left">
-                    {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
-                </ul>
-            </nav>
+            <ul class="pagination justify-content-end float-left">
+                {{ $pasiens->links('vendor.pagination.bootstrap-4') }}
+            </ul>
         </div>
     </div>
-    @endsection
+@endsection
 
 @section('script')
     @if(Auth::user()->jabatan_id == 1)
--- source/Modules/Pasien/Resources/views/show.blade.php
+++ source/Modules/Pasien/Resources/views/show.blade.php
@@ -1,76 +1,13 @@
 @extends('layouttemplate::master')
 
 @section('title')
-    Detail Pasien: {{ $pasien->nama }}
+    Rincian Pasien: {{ $pasien->nama }}
 @endsection
 
 @section('content')
-    <div class="col-md-12">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="d-none d-sm-block">
-                    <div class="card card-body">
-                        <div class="col-md-12">
-                            <h3>{{ ucwords($pasien->nama) }}</h3>
-                            <br>
-                            <table class="table" id="table">
-                                <tbody>
-                                <tr>
-                                    <th class="w-25">KTP</th>
-                                    <td>{{ $pasien->ktp }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Jenis Kelamin</th>
-                                    <td>{{ ucwords($pasien->jenkel) }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Nama Ayah/Suami</th>
-                                    <td>{{ ucwords($pasien->nama_wali) }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Alamat</th>
-                                    <td>{{ ucwords($pasien->alamat) }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Tanggal Lahir</th>
-                                    <td id="tanggal_lahir">{{ date("d F Y", strtotime($pasien->tanggal_lahir)) }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Telepon</th>
-                                    <td>{{ $pasien->telepon }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Pekerjaan</th>
-                                    <td>{{ ucfirst($pasien->pekerjaan) }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Agama</th>
-                                    <td>{{ ucfirst($pasien->agama) }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Golongan Darah</th>
-                                    <td>{{ $pasien->golongan_darah }}</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                            @if(Auth::user()->jabatan_id == 1 || Auth::user()->jabatan_id == 2)
-                                <a href="{{ route('pasien.edit', $pasien->id) }}" class="btn btn-warning float-left">Ubah Data Pasien</a>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
-
-@section('content-mobile')
-    <div class="d-block d-sm-none">
-        <div class="card card-body">
-            <h4>{{ ucwords($pasien->nama) }}</h4>
-            <br>
-            <table class="table small" id="table">
-                <tbody>
+    <div class="card card-body">
+        <table class="table table-striped" id="table">
+            <tbody>
                 <tr>
                     <th class="w-25">KTP</th>
                     <td>{{ $pasien->ktp }}</td>
@@ -107,14 +44,17 @@
                     <th>Golongan Darah</th>
                     <td>{{ $pasien->golongan_darah }}</td>
                 </tr>
-                </tbody>
-            </table>
-            @if(Auth::user()->jabatan_id == 1 || Auth::user()->jabatan_id == 2)
-                <a href="{{ route('pasien.edit', $pasien->id) }}" class="btn btn-warning float-left">Ubah Data Pasien</a>
-            @endif
-        </div>
+            </tbody>
+        </table>
+        @if(Auth::user()->jabatan_id == 1 || Auth::user()->jabatan_id == 2)
+            <div class="col-md-12">
+                <div class="row">
+                    <a href="{{ route('pasien.edit', $pasien->id) }}" class="btn btn-warning float-left">Ubah Data Pasien</a>
+                </div>
+            </div>
+        @endif
     </div>
-    @endsection
+@endsection
 
 @section('script')
     @if(Auth::user()->jabatan_id == 1)
